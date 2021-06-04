3e1c725dc6cb3d018bf2211155e0c98d7ac03b67
rafaelmartinsfranqueira <rafaelmartinsfranqueira@gmail.com>
2021-06-04 16:02:49 -0300

    heroku

--- src/main/java/com/projects/praticandoAPI/PraticandoApiApplication.java
+++ src/main/java/com/projects/praticandoAPI/PraticandoApiApplication.java
@@ -2,11 +2,9 @@ package com.projects.praticandoAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Controller
